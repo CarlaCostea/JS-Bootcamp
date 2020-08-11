fd8250ec8cbb1fd1994b6c6cbbc1eec3a794238b
Carla <carlacostea@yahoo.com>
2020-08-11 11:39:43 +0300

    http requests challenge

--- hangman/app.js
+++ hangman/app.js
@@ -28,15 +28,37 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.message
 })
 
-// Making an HTTP request
+// // Making an HTTP request
+// const request = new XMLHttpRequest()
+
+// // https://httpstatuses.com/
+// request.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         console.log(data)
+//     } else if (e.target.readyState === 4) {
+//         console.log('An error occurred')
+//     }
+// })
+
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+// request.send()
+
+// Challenge
+
+const countryCode = 'RO'
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
-        console.log(data)
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name)
+    } else if (e.target.readyState === 4) {
+        console.log('An error occurred')
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'https://restcountries.eu/rest/v2/all')
 request.send()
+
