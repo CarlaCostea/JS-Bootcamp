fae1500351774973c14bc11bc2cf90e7b410ddfe
Carla <carlacostea@yahoo.com>
2020-08-11 13:10:23 +0300

    callback abstraction

--- hangman/app.js
+++ hangman/app.js
@@ -28,37 +28,33 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.message
 })
 
-// // Making an HTTP request
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error :${error}`)
+        return
+    } 
+    console.log(puzzle)
+})
+
+// Challenge
+
+    // Making an HTTP request
+
+// const countryCode = 'RO'
 // const request = new XMLHttpRequest()
 
-// // https://httpstatuses.com/
+    // https://httpstatuses.com/
+
 // request.addEventListener('readystatechange', (e) => {
 //     if (e.target.readyState === 4 && e.target.status === 200) {
 //         const data = JSON.parse(e.target.responseText)
-//         console.log(data)
+//         const country = data.find((country) => country.alpha2Code === countryCode)
+//         console.log(country.name)
 //     } else if (e.target.readyState === 4) {
 //         console.log('An error occurred')
 //     }
 // })
 
-// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+// request.open('GET', 'https://restcountries.eu/rest/v2/all')
 // request.send()
-
-// Challenge
-
-const countryCode = 'RO'
-const request = new XMLHttpRequest()
-
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        const country = data.find((country) => country.alpha2Code === countryCode)
-        console.log(country.name)
-    } else if (e.target.readyState === 4) {
-        console.log('An error occurred')
-    }
-})
-
-request.open('GET', 'https://restcountries.eu/rest/v2/all')
-request.send()
 
