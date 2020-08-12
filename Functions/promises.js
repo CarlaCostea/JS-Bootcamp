fe3e2c3a58bb3a2f5fa2b56d1114ef56c46599dd
Carla <carlacostea@yahoo.com>
2020-08-12 10:14:29 +0300

    promise chaining

--- Functions/promises.js
+++ Functions/promises.js
@@ -1,15 +1,40 @@
-// Callback
-const getDataCallback = (callback) => {
+// // Callback
+// const getDataCallback = (callback) => {
+//     setTimeout(() => {
+//         callback(undefined, 'This time is up')
+//     }, 2000);
+// }
+
+// getDataCallback((err, data) => {
+//     if (err) {
+//         return 'error'
+//     }
+//     console.log(data)
+// })
+
+// Callback chaining
+const getDataCallback = (num, callback) => {
     setTimeout(() => {
-        callback(undefined, 'This time is up')
-    }, 2000);
+        if (typeof num === 'number') {
+            callback(undefined, num * 2)
+        } else {
+            callback('Number must be provided')
+        }
+    }, 2000)
 }
 
-getDataCallback((err, data) => {
+getDataCallback(2, (err, data) => {
     if (err) {
-        return 'error'
+        console.log(err)
+    } else {
+        getDataCallback(data, (err, data) => {
+            if (err) {
+                console.log('err')
+            } else {
+                console.log(data)
+            }
+        })
     }
-    console.log(data)
 })
 
 // simulate a delay
@@ -17,25 +42,54 @@ getDataCallback((err, data) => {
 //     console.log('This time is up')
 // }, 2000);
 
-// Promise
-const getDataPromise = (data) => new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(`This is my promise succes data: ${data}`)
-            //reject('this is my promise error')
-        }, 2000);
+// // Promise
+// const getDataPromise = (data) => new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(`This is my promise succes data: ${data}`)
+//         //reject('this is my promise error')
+//     }, 2000);
+// })
+
+
+// const myPromise = getDataPromise('someData')
+
+// myPromise.then((data) => {
+//     console.log(data)
+// }, (err) => {
+//     console.log(err)
+// })
+
+// myPromise.then((data) => {
+//     console.log(data)
+// }, (err) => {
+//     console.log(err)
+// })
+
+// Promise chaining
+const getDataPromise = (num) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        typeof num === 'number' ? resolve(num * 2) : reject('You must provide a number')
+    }, 2000);
+})
+
+
+// using nesting - not ok
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(`Promise data: ${data}`)
+    }, (err) => {
+        console.log(err)
     })
-
-
-const myPromise = getDataPromise('someData')
-
-myPromise.then((data) => {
-    console.log(data)
 }, (err) => {
     console.log(err)
 })
 
-myPromise.then((data) => {
+getDataPromise(10).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
     console.log(data)
-}, (err) => {
+}).catch((err) => {
     console.log(err)
 })
