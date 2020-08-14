3ae43ce2f77d15d884dd47add882619f15cdb149
Carla <carlacostea@yahoo.com>
2020-08-14 12:05:54 +0300

    exploring babel

---
+++ boilerplate/input.js
@@ -0,0 +1,13 @@
+// in terminat: babel input.js -o output.js then from boilerplate folder: npm init
+// local install npm install babel-preset-env
+
+//babel input.js --out-file output.js --presets env
+
+const name = 'Jen'
+console.log(name)
+
+class Hangman {
+    myMethod() {
+        return 'Testing'
+    }
+}
