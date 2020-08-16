95019d09c5fdea8840cabd1fb4b83e1f2503fbf9
Carla <carlacostea@yahoo.com>
2020-08-16 12:38:17 +0300

    webpack

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -7,6 +7,23 @@ module.exports = {
         path: path.resolve(__dirname, 'public/scripts'),
         // this prop chanegs the default file name: main
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            // target just js files
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                } 
+            }
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/scripts/'
     }
 }
 
@@ -14,3 +31,5 @@ module.exports = {
 
 // path needs an absolute path from the root of hard drive
 // /Users/carla/Documents/Work/JS-Bootcamp/boilerplate/public/scripts
+
+// to add babel with webpack : npm install babel-loader
