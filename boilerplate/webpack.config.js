6f4a6ec1f698713fd249b29a73b985837e1b8753
Carla <carlacostea@yahoo.com>
2020-08-17 10:59:52 +0300

    hangman deploy

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -3,7 +3,7 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['babel-polyfill', './src/index.js'],
     output: {
         path: path.resolve(__dirname, 'public/scripts'),
         // this prop chanegs the default file name: main
